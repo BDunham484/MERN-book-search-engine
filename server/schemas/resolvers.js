029331b84f12dfd719f218dffc5c86785d7839a2
ThereWolf <bdunham484@gmail.com>
2022-09-26 18:25:46 -0500

    add removeBook

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,7 +8,6 @@ const resolvers = {
             if (context.user) {
                 const userData = await User.findOne({ _id: context.user._id })
                     .select('-__v -password')
-                    .populate('savedBooks')
 
                 return userData;
             }
@@ -55,7 +54,7 @@ const resolvers = {
             if (context.user) {
                 const user = await User.findByIdAndUpdate(
                     { _id: context.user._id },
-                    { $pull: { savedBooks: bookId }},
+                    { $pull: { savedBooks: {bookId: bookId} }},
                     { new: true }
                 ).select('-__v')
                 return user;
