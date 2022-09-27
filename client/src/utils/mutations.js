280482ab7a37a282763b379e3743607cd3d6ec8f
ThereWolf <bdunham484@gmail.com>
2022-09-27 16:17:29 -0500

    add client/queries & mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,55 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token 
+            user {
+                _id
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $password: String!,$email: String!) {
+        addUser(username: $username, password: $password, email: $email) {
+            token
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation Mutation($bookId: ID, $authors: [String], $description: String, $title: String, $link: String, $image: String) {
+        saveBook(bookId: $bookId, authors: $authors, description: $description, title: $title, link: $link, image: $image) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation RemoveBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            username
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
